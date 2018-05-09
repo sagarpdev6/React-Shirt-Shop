3109a4a47fc93757e74232991b171e17b88ba3c7
Sagar_Patel <sagarp@dev6.com>
2018-05-09 13:32:17 -0400

    Home Component initial code.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+import { Row } from 'reactstrap';
+
+const background = require('../../images/Fractal.png');
+const logoVertical = require('../../images/Shirtastic-vertical.svg');
+
+class Home extends Component {
+
+    render() {
+        return (<div>
+            <div className="img-container">
+                <img src={background} />
+            </div>
+            <Row>
+                <div className="side-column">
+                    {/* Login Component Here */}
+                </div>
+                <div className="center-column">
+                    <img className="vertical-logo" src={logoVertical} />
+                    <div className="copyright text-center">© 2018 DEV6 – A division of The New Toronto Group Inc.</div>
+                </div>
+                <div className="side-column">
+                    {/* Signup and Shipping tabs here */}
+                </div>
+            </Row>
+        </div>);
+    }
+}
+export default Home;

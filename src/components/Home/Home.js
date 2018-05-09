dcedc51cbbc241a8368356130660ad64a7989f81
Sagar Patel <36165315+sagarpdev6@users.noreply.github.com>
2018-05-09 15:08:28 -0400

    Bootstrap tabs created for signup and shipping. (#4)

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './Home.css';
 
-import { Row } from 'reactstrap';
+import { Row, Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
+import classnames from 'classnames';
 
 import Login from '../Login/Login';
 import Signup from '../Signup/Signup';
@@ -10,6 +11,23 @@ const background = require('../../images/Fractal.png');
 const logoVertical = require('../../images/Shirtastic-vertical.svg');
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            activeTab: '1'
+        };
+    }
+
+    toggle(tab) {
+        if (this.state.activeTab !== tab) {
+            this.setState({
+                activeTab: tab
+            });
+        }
+    }
 
     render() {
         return (<div>
@@ -26,8 +44,27 @@ class Home extends Component {
                     <div className="copyright text-center">© 2018 DEV6 – A division of The New Toronto Group Inc.</div>
                 </div>
                 <div className="side-column">
-                    {/* Signup and Shipping tabs here */}
-                    <Signup />
+                    {this.state.activeTab === '1' ? <h2 className="text-center">Sign up</h2> : null}
+                    <Nav tabs>
+                        <NavItem>
+                            <NavLink className={classnames({ active: this.state.activeTab === '1' })}
+                                onClick={() => { this.toggle('1'); }}>Step 1</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink className={classnames({ active: this.state.activeTab === '2' })}
+                                onClick={() => { this.toggle('2'); }}>Step 2</NavLink>
+                        </NavItem>
+                    </Nav>
+                    <TabContent activeTab={this.state.activeTab}>
+                        <TabPane tabId="1">
+                            {/* Signup Component Goes Here */}
+                            <Signup />
+                        </TabPane>
+                        <TabPane tabId="2">
+                            {/* Shipping Component Goes Here */}
+                            Shipping Component goes here
+                    </TabPane>
+                    </TabContent>
                 </div>
             </Row>
         </div>);

e9c5cbda881b7bf87c107b1c19d3576d19f9a151
Sagar Patel <36165315+sagarpdev6@users.noreply.github.com>
2018-05-11 17:40:31 -0400

    Features/cart (#8)
    * Navbar buttons added.
    
    * Cart, SidenavShipping, Payment and Confirmation Components added.

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './Cart.css';
+import { Row, Col } from 'reactstrap';
+
+class Cart extends Component {
+    openShipping = () => {
+        this.props.openShipping();
+        this.refs.cartOverlay.style.display = "block";
+    }
+
+    closeCart = () => {
+        this.props.closeCart();
+    }
+    render() {
+        return (
+            <div>
+                <div className="cart-overlay" ref="cartOverlay"></div>
+                <div className="cart-container">
+                    <Row className="cart-header">
+                        <Col className="cart-title" xs="8">Shopping Cart</Col>
+                        <Col className="cart-btn" xs="3" onClick={() => { this.closeCart(); }}>
+                            <Row className="cart-btn-row">
+                                <div className="nav-icon-basket"></div>
+                                <div className="cart-count">3</div>
+                            </Row>
+                        </Col>
+                    </Row>
+                    <hr />
+                    <button type="button" className="primary-btn" onClick={() => { this.openShipping(); }}>GO TO SHIPPING -></button>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Cart;

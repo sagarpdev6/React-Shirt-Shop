b263fc16e97bcc5d8801de79d2090e5114d35184
Sagar Patel <36165315+sagarpdev6@users.noreply.github.com>
2018-05-22 15:53:33 -0400

    Temp design. Needs image replacement in future. (#17)

--- src/components/Shirt/Shirt.js
+++ src/components/Shirt/Shirt.js
@@ -8,13 +8,29 @@ class Shirt extends Component {
         this.props.addToCart(this.props.shirt);
     }
 
+    reCalculate = (pixels, parameter) => {
+        let newPixels;
+        pixels = pixels.substring(0, pixels.length - 2);
+        switch (parameter) {
+            case 'left':
+                newPixels = (parseInt(pixels, 10) / 678) * 100 + 5 + "%";
+                break;
+            case 'top':
+                newPixels = (parseInt(pixels, 10) / 813) * 100 + "%";
+                break;
+            default:
+                newPixels = "100px";
+        }
+        return newPixels;
+    }
+
     render() {
         return (
             <Card className="text-center">
                 {this.props.shirt.graphic ?
-                    <img ref="graphicImage" className="img-fluid shirt-graphic-img" style={{ left: this.props.shirt.textElementPosition.left, top: this.props.shirt.textElementPosition.top }} src={this.props.shirt.graphic ? require(`../../images/${this.props.shirt.graphic}`) : ''} alt="shirt graphic" /> : null}
+                    <img ref="graphicImage" className="img-fluid shirt-graphic-img" style={{ left: this.reCalculate(this.props.shirt.textElementPosition.left, 'left'), top: this.reCalculate(this.props.shirt.textElementPosition.top, 'top') }} src={this.props.shirt.graphic ? require(`../../images/${this.props.shirt.graphic}`) : ''} alt="shirt graphic" /> : null}
                 {this.props.shirt.text ?
-                    <div ref="text" className="shirt-text-final" style={{ color: this.props.shirt.textColor, fontFamily: this.props.shirt.font, left: this.props.shirt.graphicElementPosition.left, top: this.props.shirt.graphicElementPosition.top }}>{this.props.shirt.text}</div> : null}
+                    <div ref="text" className="shirt-text-final" style={{ color: this.props.shirt.textColor, fontFamily: this.props.shirt.font, left: this.reCalculate(this.props.shirt.graphicElementPosition.left, 'left'), top: this.reCalculate(this.props.shirt.graphicElementPosition.top, 'top') }}>{this.props.shirt.text}</div> : null}
                 <img className="img-fluid" src={require(`../../images/${this.props.shirt.image}`)} alt="Shirt" />
                 <h4 className="card-title">{this.props.shirt.name}</h4>
                 <p className="description">{this.props.shirt.description}</p>
